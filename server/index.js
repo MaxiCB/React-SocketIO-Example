4d451df45729938d567866bdcedc92ecf014e6b9
Aaron Beasley <aaroncb@aaroncb.com>
2020-02-19 02:03:08 -0600

    ADD Basic Drawing functionality
    - Implemented express server using SocketIO
    - Setup websocket on server
    - Created HTML-JS webpage to interact with server
    - Added functionality to draw on canvas
    - Ensured this data is sent in real time to all other clients connected to the server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const port = process.env.PORT || 1337
+
+app.use(express.static(__dirname + '/public'))
+
+const onConnection = socket => {
+    socket.on('drawing', (data) => socket.broadcast.emit('drawing', data))
+}
+
+io.on('connection', onConnection)
+http.listen(port, () => console.log('listening on port ' + port))
---
+++ server/public/main.js
@@ -0,0 +1,94 @@
+
+(function() {
+    let socket = io();
+    let canvas = document.getElementsByClassName('whiteboard')[0]
+    let colors = document.getElementsByClassName('colors')
+    let context = canvas.getContext('2d')
+
+    let current = {
+        color: 'black'
+    }
+    let drawing = false
+
+    canvas.addEventListener('mousedown', onMouseDown, false)
+    canvas.addEventListener('mouseup', onMouseUp, false)
+    canvas.addEventListener('mouseout', onMouseUp, false)
+    canvas.addEventListener('mousemove', onMouseMove, throttle(onMouseMove, 10), false)
+
+    for (let i = 0; i < colors.length; i++) {
+        colors[i] .addEventListener('click', onColorUpdate, false)
+    }
+
+    socket.on('drawing', onDrawingEvent)
+
+    window.addEventListener('resize', onResize, false)
+    onResize()
+
+    const drawLine = (x0, y0, x1, y1, color, emit) => {
+        context.beginPath();
+        context.moveTo(x0, y0)
+        context.lineTo(x1, y1)
+        context.strokeStyle = color
+        context.lineWidth = 2
+        context.stroke()
+        context.closePath()
+
+        if(!emit){return}
+        let w = canvas.width
+        let h = canvas.height
+
+        socket.emit('drawing', {
+            x0: x0 / w,
+            y0: y0 / h,
+            x1: x1 / w,
+            y1: y1 / h,
+            color: color
+        })
+    }
+
+    function onMouseDown(e){
+        drawing = true
+        current.x = e.clientX
+        current.y = e.clientY
+    }
+
+    function onMouseUp(e) {
+        if (!drawing){return}
+        drawing = false
+        drawLine(current.x, current.y, e.clientX, e.clientY, current.color, true)
+    }
+
+    function onMouseMove(e) {
+        if(!drawing){return}
+        drawLine(current.x, current.y, e.clientX, e.clientY, current.color, true)
+        current.x = e.clientX
+        current.y = e.clientY
+    }
+
+    function onColorUpdate (e) {
+        current.color = e.target.className.split(' ')[1]
+    }
+
+    function throttle(callback, delay){
+        let previous = new Date().getTime()
+        return function(){
+            let time = new Date().getTime()
+
+            if((time - previous) >= delay){
+                previous = time
+                callback.apply(null, arguments)
+            }
+        }
+    }
+
+    function onDrawingEvent(data){
+        let w = canvas.width
+        let h = canvas.height
+        drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color)
+    }
+
+    function onResize(){
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+    }
+})();
